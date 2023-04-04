a85dd9546b0f0c9163e764a4bb6d8af9bde20475
Eren169 <99626107+Eren169@users.noreply.github.com>
2023-04-04 16:56:48 +0700

    Create CTDL&GT_Quick_Sort_1.cpp

---
+++ CTDL&GT_Quick_Sort_1.cpp
@@ -0,0 +1,13 @@
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* index là chỉ số nơi phần tử này đã đứng đúng vị trí
+         và đây là phần tử chia mảng làm 2 mảng con trái & phải */
+        int index = partition(arr, low, high);
+  
+        // Gọi đệ quy sắp xếp 2 mảng con trái và phải
+        quickSort(arr, low, index - 1);
+        quickSort(arr, index + 1, high);
+    }
+}
